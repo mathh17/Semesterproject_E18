6d0af506248e46d07e50fe9e9d0818322d200ba2
Unknown <nicsi16@student.sdu.dk>
2018-11-19 13:40:26 +0100

    Boxene med ikon wiggle virker

--- js/js-nickas.js
+++ js/js-nickas.js
@@ -1,11 +1,3 @@
 $(document).ready(function () {
 
-    /*$(".bgContentBoxImage2").hover(function () {
-        $("#test").wiggle( {times:2}, 500 );
-    });*/
-    
-    /*$(".bgContentBoxImage2").hover(function () {
-        $('#wiggle').wiggle;
-    });*/
-
 });
