865bf3b031e2a1bb2c9e9f7fa49e983868dd03d5
DrunkenGenius <troelsdeibjerg@gmail.com>
2018-12-03 15:55:42 +0100

    Added slideIn boxe Quiz og Diverse

---
+++ js/interactiveStory.js
@@ -0,0 +1,18 @@
+class Dilemma {
+    constructor(dilemmaNummer, antalSvarMulighedder, billedLokation[]) {
+        this.dilemmaNummer = dilemmaNummer;
+        this.antalSvarMuliigheder = antalSvarMuligheder;
+        this.billeder[];
+
+        for (var i = 0; i < billeder.length(); i++) {
+
+            billeder[i] = new Image();
+            this.billeder[i].src = ;
+        }
+    }
+}
+
+$(document).ready(function () {
+    Dilemma dilemma1 = new Dilemma(1, 3, ['../img/beer.png', '../img/book.png', '../img/Quiz.png'])
+
+})
