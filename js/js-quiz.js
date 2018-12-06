b0e7fe54fdb14cb77343ee8d7067ee24cd29fb97
Unknown <mathh17@student.sdu.dk>
2018-12-06 12:44:28 +0100

    mysql fil

--- js/js-quiz.js
+++ js/js-quiz.js
@@ -1,29 +1,52 @@
  $(document).ready(function () {
                 // Sending data from the client via AJAX
                 var arr=[];
+                var quest = 1;
+                var a1 = 2;
+                var a2 = 3;
+                var a3 = 4;
+                var facit = 5;
                 $.ajax({
                 	type: "GET",
                     url: "http://localhost:7000/read",
                     success: function (data) {
-                        console.log(data);
-                        
-
+                        //console.log(data);
                         // Possible use of the data
                         arr = data.substr(8).split('|'); // cut first 8 char, then convert to array of strings, using '|' as separator
-                        for (var i=0;i<arr.length;i++){
+                         for (var i=0;i<arr.length;i++){
                         	console.log( i , arr[i]);
+                             
                         }
-                        console.log(arr[1]);
-                    
-                        $('#question').append(arr[1]);
-                        $('#svar1').text(arr[2]);
-                        $('#svar2').append(arr[3]);
-                        $('#svar3').append(arr[4]);
-                        $('#facit').append(arr[5]);
+                        loadQuestion();
                     },
                     error: function (data) {
                         console.log("error!");
                     },
                     
                 });
+                
+                function loadQuestion (){
+                
+                    
+                        $('#question').text(arr[quest]);
+                        $('#svar1').text(arr[a1]);
+                        $('#svar2').text(arr[a2]);
+                        $('#svar3').text(arr[a3]);
+                        $('#facit').text(arr[facit]);
+                    
+                }
+                           
+                function nextQuestion (){
+                    quest = quest + 6;
+                    a1 = a1 + 6;
+                    a2 = a2 + 6;
+                    a3 = a3 + 6;
+                    facit = facit +6;
+                    loadQuestion();
+                }
+                
+                $('#svar1').click(function(){
+                   nextQuestion(); 
+                });
+                
             });
