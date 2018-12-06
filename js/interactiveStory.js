92a295cef2e0e6720fa479cf7611b5785817142d
DrunkenGenius <troelsdeibjerg@gmail.com>
2018-12-06 14:08:08 +0100

    2 valg knapper når der er to valg og 3 når der er 3

--- js/interactiveStory.js
+++ js/interactiveStory.js
@@ -1,7 +1,7 @@
 class Dilemma {
     constructor(antalSvarMuligheder, billedLokation, svarBilleder) {
         this.antalSvarMuligheder = antalSvarMuligheder;
-        this.billedLokation = billedLokation;
+        this.sceneBilleder = billedLokation;
         this.svarBilleder = svarBilleder;
         this.billeder = [''];
     }
@@ -9,14 +9,14 @@ class Dilemma {
 
 $(document).ready(function () {
 
-    $("#valgBtn").hide();
+    $("#valgBtn3, #valgBtn2").hide();
     let dilemma1 = new Dilemma(3, ['../dilemmaer/intro/INTROHISTORIE_JPG.jpg', '../dilemmaer/intro/INTROHISTORIE2_JPG.jpg', '../dilemmaer/intro/INTROHISTORIE3_JPG.jpg', '../dilemmaer/intro/INTROHISTORIE4_JPG.jpg'], ['../dilemmaer/dilemma1/DILEMMA1_SVAR_1_JPG.jpg', '../dilemmaer/dilemma1/DILEMMA1_SVAR_2_JPG.jpg', '../dilemmaer/dilemma1/DILEMMA1_SVAR_3_JPG.jpg']);
     let dilemma2 = new Dilemma(3, ['../dilemmaer/dilemma1/DILEMMA1_JPG.jpg', '../dilemmaer/dilemma1/DILEMMA1.1_JPG.jpg', '../dilemmaer/dilemma1/DILEMMA1.2_JPG.jpg'], ['../dilemmaer/dilemma1/DILEMMA1_SVAR_1_JPG.jpg', '../dilemmaer/dilemma1/DILEMMA1_SVAR_2_JPG.jpg', '../dilemmaer/dilemma1/DILEMMA1_SVAR_3_JPG.jpg']);
-    let dilemma3 = new Dilemma(3, ['../dilemmaer/dilemma1/INTROHISTORIE_JPG.jpg', '../dilemmaer/dilemma1/INTROHISTORIE2_JPG.jpg', '../dilemmaer/dilemma1/INTROHISTORIE3_JPG.jpg', '../dilemmaer/dilemma1/INTROHISTORIE4_JPG.jpg'], ['../dilemmaer/dilemma1/DILEMMA1_SVAR_1_JPG.jpg', '../dilemmaer/dilemma1/DILEMMA1_SVAR_2_JPG.jpg', '../dilemmaer/dilemma1/DILEMMA1_SVAR_3_JPG.jpg']);
+    let dilemma3 = new Dilemma(3, ['../dilemmaer/dilemma1/DILEMMA1_JPG.jpg', '../dilemmaer/dilemma1/DILEMMA1.1_JPG.jpg', '../dilemmaer/dilemma1/DILEMMA1.2_JPG.jpg'], ['../dilemmaer/dilemma1/DILEMMA1_SVAR_1_JPG.jpg', '../dilemmaer/dilemma1/DILEMMA1_SVAR_2_JPG.jpg']);
     let dilemma4 = new Dilemma(3, ['../dilemmaer/dilemma1/INTROHISTORIE_JPG.jpg', '../dilemmaer/dilemma1/INTROHISTORIE2_JPG.jpg', '../dilemmaer/dilemma1/INTROHISTORIE3_JPG.jpg', '../dilemmaer/dilemma1/INTROHISTORIE4_JPG.jpg'], ['../dilemmaer/dilemma1/DILEMMA1_SVAR_1_JPG.jpg', '../dilemmaer/dilemma1/DILEMMA1_SVAR_2_JPG.jpg', '../dilemmaer/dilemma1/DILEMMA1_SVAR_3_JPG.jpg']);
     let dilemma5 = new Dilemma(3, ['../dilemmaer/dilemma1/INTROHISTORIE_JPG.jpg', '../dilemmaer/dilemma1/INTROHISTORIE2_JPG.jpg', '../dilemmaer/dilemma1/INTROHISTORIE3_JPG.jpg', '../dilemmaer/dilemma1/INTROHISTORIE4_JPG.jpg'], ['../dilemmaer/dilemma1/DILEMMA1_SVAR_1_JPG.jpg', '../dilemmaer/dilemma1/DILEMMA1_SVAR_2_JPG.jpg', '../dilemmaer/dilemma1/DILEMMA1_SVAR_3_JPG.jpg']);
     var dilemmaPosition = 0;
-    var scenePosition = 1;
+    var scenePosition = 0;
     var dilemmaPosition = 1;
 
 
@@ -54,9 +54,13 @@ $(document).ready(function () {
     });
 
     //Sørger for at valg knapperne skifter til dit valg efter hvilken knap du klikker på, gøres ved at finde index af knappen du trykker på
-    $("#valgBtn button").click(function () {
+    $("#valgBtn3 button, #valgBtn2 button").click(function () {
         //her findes index af knappen
-        var valgtNummer = $("#valgBtn button").index(this);
+        var valgtNummer = $("#valgBtn3 button, #valgBtn2 button").index(this);
+        //Hvis der kun er 2 valg, trækkes tre fra index fordi den tæller de tre første knapper og derfor får index
+        if(valgtNummer>2){
+            valgtNummer -= 3;
+        }
         $("img").attr("src", dilemmaer[dilemmaPosition].svarBilleder[valgtNummer]);
     });
 
@@ -66,40 +70,52 @@ $(document).ready(function () {
 
     //Bygger dilemmaerne når der kligges på forrige dilemma og næste dilemma knapper
     function BuildDilemma(dilemma) {
-        $("img").attr("src", dilemma.billedLokation[0]);
+        $("img").attr("src", dilemma.sceneBilleder[0]);
     }
 
     //Shower Valg knapper og Hider frem/tilbage knapper
     function AktiverValgKnapper() {
         $("#buttons").hide();
-        $("#valgBtn").show();
+
+        //Tjekker om der er 2 eller 3 valg
+        if (dilemmaer[dilemmaPosition].svarBilleder.length == 3) {
+            $("#valgBtn3").show();
+        } else {
+            $("#valgBtn2").show();
+        }
     }
 
     function AktiverFremTilbageKnapper() {
         $("#buttons").show();
-        $("#valgBtn").hide();
+
+        //Tjekker om der er 2 eller 3 valg
+        if (dilemmaer[dilemmaPosition].svarBilleder.length == 2) {
+            $("#valgBtn3").hide();
+        } else {
+            $("#valgBtn2").hide();
+        }
     }
 
     function NæsteScene() {
         //Skifter scenen indtil du er på sidste billede i historie arrayet. Det sidste billede er billedet med forskellige svar.
-        if (scenePosition < dilemmaer[dilemmaPosition].billedLokation.length - 1) {
+        if (scenePosition < dilemmaer[dilemmaPosition].sceneBilleder.length - 1) {
             scenePosition++;
-            $("img").attr("src", dilemmaer[dilemmaPosition].billedLokation[scenePosition]);
+            $("img").attr("src", dilemmaer[dilemmaPosition].sceneBilleder[scenePosition]);
         } else
             scenePosition++;
-        console.log(dilemmaer[dilemmaPosition].billedLokation.length + " og " + scenePosition);
+        console.log(dilemmaer[dilemmaPosition].sceneBilleder.length + " og " + scenePosition);
 
-        // her har vi ramt billedet med svar på og aktiverer altså Valg Knapperne 
-        if (scenePosition == dilemmaer[dilemmaPosition].billedLokation.length) {
+        // her rammer vi billedet med svar på og aktiverer altså Valg Knapperne 
+        if (scenePosition == dilemmaer[dilemmaPosition].sceneBilleder.length - 1) {
             AktiverValgKnapper();
-            $("img").attr("src", dilemmaer[dilemmaPosition].billedLokation[0]);
+            $("img").attr("src", dilemmaer[dilemmaPosition].sceneBilleder[scenePosition]);
         }
     }
 
     function ForrigeScene() {
-        if (scenePosition < dilemmaer[dilemmaPosition].billedLokation.length) {
+        if (scenePosition < dilemmaer[dilemmaPosition].sceneBilleder.length) {
             scenePosition--;
-            $("img").attr("src", dilemmaer[dilemmaPosition].billedLokation[scenePosition]);
+            $("img").attr("src", dilemmaer[dilemmaPosition].sceneBilleder[scenePosition]);
         }
     }
 
