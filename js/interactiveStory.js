d9dce917e6b97750fa38e03821a7a362e9333222
DrunkenGenius <troelsdeibjerg@gmail.com>
2018-12-12 16:41:00 +0100

    donesoooooo

--- js/interactiveStory.js
+++ js/interactiveStory.js
@@ -6,26 +6,26 @@ class Dilemma {
 }
 
 $(document).ready(function () {
-
+    $("#knapTilToSvarSider").hide();
     $("#valgBtn3, #valgBtn2").hide();
     $("#prøvIgenOgFremadBtn").hide();
     $("#prøvForFraBtn").hide();
-    let dilemma1 = new Dilemma(['../dilemmaer/Dilemma1/INTRO_JPG.jpg','../dilemmaer/Dilemma1/INTROHISTORIE_JPG.jpg','../dilemmaer/Dilemma1/INTROHISTORIE2_JPG.jpg','../dilemmaer/Dilemma1/INTROHISTORIE3_JPG.jpg','../dilemmaer/Dilemma1/INTROHISTORIE4_JPG.jpg','../dilemmaer/Dilemma1/DILEMMA1_JPG.jpg','../dilemmaer/Dilemma1/DILEMMA1.1_JPG.jpg','../dilemmaer/Dilemma1/DILEMMA1.2_JPG.jpg'],['../dilemmaer/Dilemma1/DILEMMA1_SVAR_1_JPG.jpg','../dilemmaer/Dilemma1/DILEMMA1_SVAR_2_JPG.jpg','../dilemmaer/Dilemma1/DILEMMA1_SVAR_3_JPG.jpg']);
-    
-    
-    let dilemma2 = new Dilemma(['../dilemmaer/dilemma2/DILEMMA2_intro_JPG.jpg','../dilemmaer/dilemma2/DILEMMA2.2_JPG.jpg'], ['../dilemmaer/dilemma2/DILEMMA2._SVAR1_JPG.jpg','../dilemmaer/dilemma2/DILEMMA2._SVAR2_JPG.jpg','../dilemmaer/dilemma2/DILEMMA2._SVAR3_JPG.jpg']);
-    
-    let dilemma3 = new Dilemma(['../dilemmaer/dilemma3/DILEMMA3_intro_JPG.jpg', '../dilemmaer/dilemma3/DILEMMA3.2_JPG.jpg'], ['../dilemmaer/dilemma3/DILEMMA3_SVAR1_JPG.jpg', '../dilemmaer/dilemma3/DILEMMA3_SVAR2_JPG.jpg','../dilemmaer/dilemma3/DILEMMA3_SVAR3_JPG1.jpg']);
-    
-    let dilemma4 = new Dilemma(['../dilemmaer/dilemma1/INTROHISTORIE_JPG.jpg', '../dilemmaer/dilemma1/INTROHISTORIE2_JPG.jpg', '../dilemmaer/dilemma1/INTROHISTORIE3_JPG.jpg', '../dilemmaer/dilemma1/INTROHISTORIE4_JPG.jpg'],[""]);
-    
-    let dilemma5 = new Dilemma(['../dilemmaer/dilemma1/INTROHISTORIE_JPG.jpg', '../dilemmaer/dilemma1/INTROHISTORIE2_JPG.jpg', '../dilemmaer/dilemma1/INTROHISTORIE3_JPG.jpg', '../dilemmaer/dilemma1/INTROHISTORIE4_JPG.jpg'], ['../dilemmaer/dilemma1/DILEMMA1_SVAR_1_JPG.jpg', '../dilemmaer/dilemma1/DILEMMA1_SVAR_2_JPG.jpg', '../dilemmaer/dilemma1/DILEMMA1_SVAR_3_JPG.jpg']);
-    
-    var dilemmaPosition = 0;
-    var scenePosition = 0;
-    var dilemmaPosition = 0;
+    let dilemma1 = new Dilemma(['../dilemmaer/Dilemma1/INTRO_JPG.jpg', '../dilemmaer/Dilemma1/INTROHISTORIE_JPG.jpg', '../dilemmaer/Dilemma1/INTROHISTORIE2_JPG.jpg', '../dilemmaer/Dilemma1/INTROHISTORIE3_JPG.jpg', '../dilemmaer/Dilemma1/INTROHISTORIE4_JPG.jpg', '../dilemmaer/Dilemma1/DILEMMA1_JPG.jpg', '../dilemmaer/Dilemma1/DILEMMA1.1_JPG.jpg', '../dilemmaer/Dilemma1/DILEMMA1.2_JPG.jpg'], ['../dilemmaer/Dilemma1/DILEMMA1_SVAR_1_JPG.jpg', '../dilemmaer/Dilemma1/DILEMMA1_SVAR_2_JPG.jpg', '../dilemmaer/Dilemma1/DILEMMA1_SVAR_3_JPG.jpg']);
 
-    var dilemmaer = [dilemma1, dilemma2, dilemma3, dilemma4, dilemma5]
+
+    let dilemma2 = new Dilemma(['../dilemmaer/dilemma2/DILEMMA2_intro_JPG.jpg', '../dilemmaer/dilemma2/DILEMMA2.2_JPG.jpg'], ['../dilemmaer/dilemma2/DILEMMA2._SVAR1_JPG.jpg', '../dilemmaer/dilemma2/DILEMMA2._SVAR2_JPG.jpg', '../dilemmaer/dilemma2/DILEMMA2._SVAR3_JPG.jpg']);
+
+    let dilemma3 = new Dilemma(['../dilemmaer/dilemma3/DILEMMA3_intro_JPG.jpg', '../dilemmaer/dilemma3/DILEMMA3.2_JPG.jpg'], ['../dilemmaer/dilemma3/DILEMMA3_SVAR1_JPG.jpg', '../dilemmaer/dilemma3/DILEMMA3_SVAR2_JPG.jpg', '../dilemmaer/dilemma3/DILEMMA3_SVAR3_JPG1.jpg']);
+
+    let dilemma4 = new Dilemma(['../dilemmaer/dilemma4/DILEMMA4__Intro_Jpg.jpg', '../dilemmaer/dilemma4/4_valg_jpg.jpg'], ['../dilemmaer/dilemma4/4_svar1.jpg', '../dilemmaer/dilemma4/4_svar1.2_jpg.jpg', '../dilemmaer/dilemma4/4_svar2_jpg.jpg']);
+
+    let dilemma5 = new Dilemma(['../dilemmaer/dilemma5/Intro_jpg.jpg','../dilemmaer/dilemma5/5_muligheder_jpg.jpg'], ['../dilemmaer/dilemma5/5_svar1_jpg.jpg', '../dilemmaer/dilemma5/5_svar2_jpg.jpg']);
+    let dilemma6 = new Dilemma(['../dilemmaer/dilemma5/end.jpg'], ['']);
+
+    var scenePosition = 0;
+    var dilemmaPosition = 4;
+
+    var dilemmaer = [dilemma1, dilemma2, dilemma3, dilemma4, dilemma5, dilemma6]
     /* $('#canvas').css('background-image', 'url(' + dilemmaer[0].billedLokation[0] + ')');*/
     BuildDilemma(dilemmaer[dilemmaPosition]);
 
@@ -33,18 +33,14 @@ $(document).ready(function () {
         if (dilemmaPosition < dilemmaer.length - 1) {
             scenePosition = 0;
             dilemmaPosition++;
-            AktiverFremTilbageKnapper();
             BuildDilemma(dilemmaer[dilemmaPosition]);
-            $("#prøvIgenOgFremadBtn").hide();
         }
     });
 
     $("#forrigeDilemma").click(function () {
-        if (dilemmaPosition > 0) {
+        if (dilemmaPosition >= 0) {
             scenePosition = 0;
-            AktiverFremTilbageKnapper();
             BuildDilemma(dilemmaer[dilemmaPosition]);
-            $("#prøvIgenOgFremadBtn").hide();
         }
     });
 
@@ -67,22 +63,51 @@ $(document).ready(function () {
         }
         $("#prøvIgenOgFremadBtn").show();
         $("#valgBtn3, #valgBtn2").hide();
-        changePicture(dilemmaer[dilemmaPosition].svarBilleder[valgtNummer]);   
+        changePicture(dilemmaer[dilemmaPosition].svarBilleder[valgtNummer]);
+
+
+        //Hardcode fix til dilemma 4 hvor der er 2 svar billeder til et enkelt svar.
+        if (dilemmaPosition == 3 && valgtNummer == 0) {
+
+            $("#knapTilToSvarSider").show();
+            $("#prøvIgenOgFremadBtn").hide();
+            $("#næsteSvarBillede").click(function () {
+                changePicture(dilemma4.svarBilleder[1]);
+                $("#prøvIgenOgFremadBtn").show();
+                $("#knapTilToSvarSider").hide();
+            });
+        } else if(dilemmaPosition == 3 && valgtNummer == 1){
+            changePicture(dilemma4.svarBilleder[2]);
+        }
+
+
     });
 
-    function changePicture(billede)
-    {
+    function changePicture(billede) {
+
+
         $("button").fadeOut(100);
-        $("img").slideUp("fast",function(){
-           $("img").attr("src", billede);
+        $("img").slideUp("fast", function () {
+            $("img").attr("src", billede);
             $("img").slideDown();
             $("button").fadeIn();
-        })    
+        })
     }
 
     //Bygger dilemmaerne når der kligges på forrige dilemma og næste dilemma knapper
     function BuildDilemma(dilemma) {
-        changePicture(dilemma.sceneBilleder[0]);
+        if (dilemmaPosition != dilemmaer.length - 1) {
+            AktiverFremTilbageKnapper();
+            $("#prøvIgenOgFremadBtn").hide();
+            changePicture(dilemma.sceneBilleder[0]);
+
+        } else {
+            changePicture('../dilemmaer/dilemma5/end.jpg');
+            $('#prøvIgenOgFremadBtn').hide();
+            $('#prøvForFraBtn').show();
+        }
+        //Else't er til slut picture
+
     }
 
     //Shower Valg knapper og Hider frem/tilbage knapper
@@ -93,6 +118,12 @@ $(document).ready(function () {
         if (dilemmaer[dilemmaPosition].svarBilleder.length == 3) {
             $("#valgBtn3").show();
         } else {
+            $("#valgBtn2").show();
+        }
+        
+        //hardcode fix til dilemma 4 hvor der er to svar billeder til et svar.
+        if (dilemmaPosition == 3 && scenePosition ==1) {
+            $("#valgBtn3").hide();
             $("#valgBtn2").show();
         }
     }
@@ -110,29 +141,27 @@ $(document).ready(function () {
 
     function NæsteScene() {
         //Skifter scenen indtil du er på sidste billede i historie arrayet. Det sidste billede er billedet med forskellige svar.
+
         if (scenePosition < dilemmaer[dilemmaPosition].sceneBilleder.length - 1) {
             scenePosition++;
             changePicture(dilemmaer[dilemmaPosition].sceneBilleder[scenePosition]);
         } else
             scenePosition++;
-        console.log(dilemmaer[dilemmaPosition].sceneBilleder.length + " og " + scenePosition);
 
-        console.log(dilemmaer[dilemmaPosition].svarBilleder);
         // her rammer vi billedet med svar på og aktiverer altså Valg Knapperne 
-        if (scenePosition == dilemmaer[dilemmaPosition].sceneBilleder.length - 1 &&  dilemmaer[dilemmaPosition].svarBilleder[0] != "") {
+        if (scenePosition == dilemmaer[dilemmaPosition].sceneBilleder.length - 1 && dilemmaer[dilemmaPosition].svarBilleder[0] != "") {
             AktiverValgKnapper();
             changePicture(dilemmaer[dilemmaPosition].sceneBilleder[scenePosition]);
-        }else if(scenePosition == dilemmaer[dilemmaPosition].sceneBilleder.length - 1 && dilemmaer[dilemmaPosition].svarBilleder[0] == ""){
-            $("#prøvForFraBtn").show();
-            $("#buttons").hide();
-            
-        }
+            }
+
     }
 
     function ForrigeScene() {
-        if (scenePosition < dilemmaer[dilemmaPosition].sceneBilleder.length) {
+        if (scenePosition < dilemmaer[dilemmaPosition].sceneBilleder.length && scenePosition != 0) {
             scenePosition--;
-            changePicture(dilemmaer[dilemmaPosition].sceneBilleder[scenePosition]);
+            
         }
+        changePicture(dilemmaer[dilemmaPosition].sceneBilleder[scenePosition]);
+        
     }
 })
